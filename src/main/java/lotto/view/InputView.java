0e1a0af44710a36c9059e7982783011e945ff4b6
dmswl6310 <dmswl6310@gmail.com>
2023-11-29 10:38:28 +0900

    feat(InputView): 구매금액에 대한 예외추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,7 +7,35 @@ import java.util.stream.Collectors;
 
 public class InputView {
     public static int readPurchaseAmount() {
-        return Integer.parseInt(Console.readLine());
+        try {
+            int purchaseAmount = convertStringToInt(readNumber());
+            validateAmountOverMax(purchaseAmount);
+            validateAmountDivisible(purchaseAmount);
+            return purchaseAmount;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readPurchaseAmount();
+        }
+    }
+
+    public static int convertStringToInt(String num) {
+        try {
+            return Integer.parseInt(num);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("입력된 수는 정수여야 합니다");
+        }
+    }
+
+    public static void validateAmountOverMax(int purchaseAmount) {
+        if (purchaseAmount > 10000000) {
+            throw new IllegalArgumentException("입력된 수는 1000만원 이하여야 합니다.");
+        }
+    }
+
+    public static void validateAmountDivisible(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("입력된 수는 1000으로 나누어 떨어져야 합니다.");
+        }
     }
 
     public static List<Integer> readWinningNumbers() {
@@ -18,5 +46,9 @@ public class InputView {
 
     public static int readBonusNumber() {
         return Integer.parseInt(Console.readLine());
+    }
+
+    public static String readNumber() {
+        return Console.readLine();
     }
 }
