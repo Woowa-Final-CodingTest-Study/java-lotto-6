789957c8c1f39c0b4e3f008397c35463d976c81d
Chenowoo <cjsgusdn14@naver.com>
2023-11-30 01:59:09 +0900

    refactor(lotto): 메서드 분리 및 상수 수정

--- src/main/java/lotto/constant/MessageConstant.java
+++ src/main/java/lotto/constant/MessageConstant.java
@@ -4,7 +4,7 @@ public class MessageConstant {
     public static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     public static final String PURCHASED_LOTTO_AMOUNT = "\n%d개를 구매했습니다.\n";
     public static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요";
-    public static final String INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해주세요.";
+    public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해주세요.";
     public static final String STATISTICS = """
             \n당첨 통계
             ---
--- src/main/java/lotto/controller/LottoRankingController.java
+++ src/main/java/lotto/controller/LottoRankingController.java
@@ -26,20 +26,28 @@ public class LottoRankingController {
 
     public Map<String, Integer> compareLottoNumbers(LottoNumbers lottoNumbers, Lotto winningLotto, int bonusNumber) {
         Map<String, Integer> ranking = lottoRank.initializeRank();
-
         List<Integer> winningNumbers = winningLotto.getEachLottoNumbers();
+
         for(int i=0; i<lottoNumbers.getSize(); i++){
             Lotto lotto = lottoNumbers.getEachLotto(i);
             List<Integer> numbers = lotto.getEachLottoNumbers();
 
-            int matchingCount = lottoMatching.matchingLottoNumbers(numbers, winningNumbers);
-            boolean existBonus = lottoMatching.matchingBonusNumber(numbers, bonusNumber);
-
-            String rank = lottoRank.rankLotto(matchingCount, existBonus);
-
-            ranking.put(rank, ranking.getOrDefault(rank, 0)+1);
+            updateRanking(ranking, numbers, winningNumbers, bonusNumber);
         }
         return ranking;
+    }
+
+    public void updateRanking(
+            Map<String, Integer> ranking,
+            List<Integer> numbers,
+            List<Integer> winningNumbers,
+            int bonusNumber) {
+        int matchingCount = lottoMatching.matchingLottoNumbers(numbers, winningNumbers);
+        boolean existBonus = lottoMatching.matchingBonusNumber(numbers, bonusNumber);
+
+        String rank = lottoRank.rankLotto(matchingCount, existBonus);
+
+        ranking.put(rank, ranking.getOrDefault(rank, 0)+1);
     }
 
 }
