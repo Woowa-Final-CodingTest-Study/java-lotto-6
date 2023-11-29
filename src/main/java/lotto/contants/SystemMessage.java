3fe33dcffd2c71ebab9e5402236f30f71a416d60
dmswl6310 <dmswl6310@gmail.com>
2023-11-29 16:14:05 +0900

    fix(SystemMessage): 잘못된 시스템 메세지 수정

--- src/main/java/lotto/contants/SystemMessage.java
+++ src/main/java/lotto/contants/SystemMessage.java
@@ -2,6 +2,6 @@ package lotto.contants;
 
 public class SystemMessage {
     public static final String REQUEST_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    public static final String REQUEST_WINNING_NUMBER = "구입금액을 입력해 주세요.";
+    public static final String REQUEST_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     public static final String REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 }
