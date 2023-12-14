8a0bbb4d7893b9127ea5c11208b72e9fbc4c1398
Chenowoo <cjsgusdn14@naver.com>
2023-12-15 00:32:30 +0900

    feat(InputService): 보너스 번호를 잘못 입력시 반복해서 물어보는 기능 구현

--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -1,14 +1,17 @@
 package lotto.service;
 
+import static lotto.constant.SystemMessage.INPUT_BONUS_NUMBER;
 import static lotto.constant.SystemMessage.INPUT_PURCHASE_AMOUNT;
 import static lotto.constant.SystemMessage.INPUT_WIN_NUMBERS;
 
 import java.util.List;
+import lotto.util.InputValidation;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class InputService {
 
+    InputValidation inputValidation = new InputValidation();
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
 
@@ -18,7 +21,7 @@ public class InputService {
             outputView.print(INPUT_PURCHASE_AMOUNT.getMessage());
             int purchaseAmount = inputView.inputPurchaseAmount();
             return purchaseAmount;
-        } catch(IllegalArgumentException illegalArgumentException) {
+        } catch (IllegalArgumentException illegalArgumentException) {
             System.out.println(illegalArgumentException.getMessage());
             return getPurchaseAmount();
         }
@@ -29,9 +32,21 @@ public class InputService {
             outputView.print(INPUT_WIN_NUMBERS.getMessage());
             List<Integer> winNumbers = inputView.inputWinNumbers();
             return winNumbers;
-        } catch(IllegalArgumentException illegalArgumentException) {
+        } catch (IllegalArgumentException illegalArgumentException) {
             System.out.println(illegalArgumentException.getMessage());
             return getWinNumbers();
+        }
+    }
+
+    public int getBonusNumber(List<Integer> winNumbers) {
+        try {
+            outputView.print(INPUT_BONUS_NUMBER.getMessage());
+            int bonusNumber = inputView.inputBonusNumber();
+            inputValidation.validateDuplicateWithWinNumbers(bonusNumber, winNumbers);
+            return bonusNumber;
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException.getMessage());
+            return getBonusNumber(winNumbers);
         }
     }
 }
