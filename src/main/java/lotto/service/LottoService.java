c15d66b5474d6199623ac62d3f7e8bf2cc294e0a
Chenowoo <cjsgusdn14@naver.com>
2023-12-15 00:08:04 +0900

    feat(LottoService): 생성된 로또 번호를 출력하는 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -8,6 +8,7 @@ import static lotto.constant.NumberConstant.PURCHASE_UNIT;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import lotto.domain.Lotto;
 import lotto.repository.LottoRepository;
 
@@ -15,9 +16,7 @@ public class LottoService {
 
     LottoRepository lottoRepository = new LottoRepository();
 
-    public void generateLottoNumbers(int purchaseNumber) {
-        int purchaseAmount = purchaseNumber / PURCHASE_UNIT.getValue();
-
+    public void generateLottoNumbers(int purchaseAmount) {
         while (purchaseAmount-- > 0) {
             Lotto lotto = new Lotto(createLottoNumbers());
             lottoRepository.saveLotto(lotto);
@@ -29,5 +28,19 @@ public class LottoService {
                 MAX_LOTTO_NUMBER.getValue(), MAX_LOTTO_COUNT.getValue());
         Collections.sort(numbers);
         return numbers;
+    }
+
+    public StringBuilder getLottoNumbers() {
+        StringBuilder sb = new StringBuilder();
+        List<Lotto> lottos = lottoRepository.getLotto();
+
+        for(Lotto lotto : lottos) {
+            List<Integer> numbers = lotto.getNumbers();
+            sb.append("[")
+                    .append(numbers.stream().map(Object::toString).collect(Collectors.joining(", ")))
+                    .append("]")
+                    .append("\n");
+        }
+        return sb;
     }
 }
