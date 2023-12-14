634561daafc66d3e015b380e4cecde5f93b0d238
Chenowoo <cjsgusdn14@naver.com>
2023-12-14 23:35:52 +0900

    feat(InputValidation): 구입금액을 검증하는 기능 구현

---
+++ src/main/java/lotto/util/InputValidation.java
@@ -0,0 +1,58 @@
+package lotto.util;
+
+import static lotto.constant.ErrorMessage.INPUT_AMOUNT_UNIT;
+import static lotto.constant.ErrorMessage.INPUT_NULL;
+import static lotto.constant.ErrorMessage.INPUT_NUMBER;
+import static lotto.constant.ErrorMessage.INPUT_POSITIVE_NUMBER;
+import static lotto.constant.ErrorMessage.LIMIT_PURCHASE_AMOUNT;
+import static lotto.constant.NumberConstant.MAX_PURCHASE_AMOUNT;
+import static lotto.constant.NumberConstant.PURCHASE_UNIT;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputValidation {
+
+    public int validatePurchaseAmount(String input) {
+        validateNull(input);
+        int number = validateNumeric(input);
+        validateNegative(number);
+        validateUnit(number);
+        validateLimit(number);
+
+        return number;
+    }
+
+    private void validateNull(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(INPUT_NULL.getMessage());
+        }
+    }
+
+    private int validateNumeric(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            return number;
+        } catch (IllegalArgumentException illegalArgumentException) {
+            throw new IllegalArgumentException(INPUT_NUMBER.getMessage());
+        }
+    }
+
+    private void validateNegative(int input) {
+        if (input < 0) {
+            throw new IllegalArgumentException(INPUT_POSITIVE_NUMBER.getMessage());
+        }
+    }
+
+    private void validateUnit(int input) {
+        if (input % PURCHASE_UNIT.getValue() != 0) {
+            throw new IllegalArgumentException(String.format(INPUT_AMOUNT_UNIT.getMessage(), PURCHASE_UNIT.getValue()));
+        }
+    }
+
+    private void validateLimit(int input) {
+        if (input > MAX_PURCHASE_AMOUNT.getValue()) {
+            throw new IllegalArgumentException(
+                    String.format(LIMIT_PURCHASE_AMOUNT.getMessage(), MAX_PURCHASE_AMOUNT.getValue()));
+        }
+    }
+}
