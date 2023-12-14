6bad23ef29f020fd0ba9df91c5fd50d86737ab15
Chenowoo <cjsgusdn14@naver.com>
2023-12-15 00:24:39 +0900

    feat(InputService): 당첨 번호를 잘못된 값을 입력시 반복해서 물어보는 기능 구현

--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -1,7 +1,9 @@
 package lotto.service;
 
 import static lotto.constant.SystemMessage.INPUT_PURCHASE_AMOUNT;
+import static lotto.constant.SystemMessage.INPUT_WIN_NUMBERS;
 
+import java.util.List;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -20,6 +22,16 @@ public class InputService {
             System.out.println(illegalArgumentException.getMessage());
             return getPurchaseAmount();
         }
+    }
 
+    public List<Integer> getWinNumbers() {
+        try {
+            outputView.print(INPUT_WIN_NUMBERS.getMessage());
+            List<Integer> winNumbers = inputView.inputWinNumbers();
+            return winNumbers;
+        } catch(IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException.getMessage());
+            return getWinNumbers();
+        }
     }
 }
