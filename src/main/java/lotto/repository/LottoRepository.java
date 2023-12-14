5bfbfdb8db148fe8ce0bbe8d6dbeb3f4c42f82f4
Chenowoo <cjsgusdn14@naver.com>
2023-12-14 23:50:26 +0900

    feat(LottoRepository): 랜덤으로 뽑은 로또 번호를 저장하는 클래스 및 저장하는 기능 구현

---
+++ src/main/java/lotto/repository/LottoRepository.java
@@ -0,0 +1,14 @@
+package lotto.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+
+public class LottoRepository {
+
+    private final List<Lotto> lottoRepository = new ArrayList<>();
+
+    public void saveLotto(Lotto lotto) {
+        lottoRepository.add(lotto);
+    }
+}
